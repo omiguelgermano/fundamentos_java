31a85329b0d4c3d7681c21569364435c30de8021
Miguel Germano <miguelgermano86@gmail.com>
2022-12-26 11:53:45 -0300

    Atalhos para Eclipce e TipoString

---
+++ fundamentos/src/fundamentosPrimarios/TipoString.java
@@ -0,0 +1,13 @@
+package fundamentosPrimarios;
+
+public class TipoString {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Olá Pessoal!".charAt(2));
+		
+		String s = "Boa tarde";
+		System.out.println(s.concat("!!!"));
+		System.out.println(s + "!!!");
+	}
+}
